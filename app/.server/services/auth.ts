41ea1b07ea466c607f85660d96d8e56d6456316f
Electroww <david.merwan@gmail.com>
2024-04-23 10:27:28 +0200

    chore: add login

--- app/.server/services/auth.ts
+++ app/.server/services/auth.ts
@@ -1,9 +1,11 @@
 import { CreateUserDto } from '@/types/createUserDto';
 import supabaseAuth from '../database/auth';
+import { LoginUserDto } from '@/types/loginUserDto';
 
 enum ErrorCodes {
   WEAK_PASSWORD = 'weak_password',
   USER_ALREADY_EXISTS = 'user_already_exists',
+  INVALID_CREDENTIALS = 'invalid_credentials',
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -18,4 +20,15 @@ const createUser = async ({ fullname, email, password }: CreateUserDto) => {
   return data;
 };
 
-export { createUser, ErrorCodes };
+const loginUser = async ({ email, password }: LoginUserDto) => {
+  const { data, error } = await supabaseAuth.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (error !== null) {
+    return { error: ErrorCodes.INVALID_CREDENTIALS };
+  }
+  return data;
+};
+
+export { createUser, loginUser, ErrorCodes };
--- app/routes/_auth.login.tsx
+++ app/routes/_auth.login.tsx
@@ -1,22 +1,31 @@
+import { ErrorCodes, loginUser } from '@/.server/services/auth';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Form } from '@remix-run/react';
+import { commitSession, getSession } from '@/sessions';
+import { LoginUserDto } from '@/types/loginUserDto';
+import { ActionFunctionArgs } from '@remix-run/node';
+import { Form, json, redirect, useActionData } from '@remix-run/react';
 
-export default function login() {
+export default function Login() {
+  const actionData = useActionData<typeof action>();
+
   return (
     <Form method="post" className="flex flex-col gap-16">
       <div className="flex flex-col gap-3 pt-5">
         <h1 className="text-sm font-black uppercase">Reset</h1>
         <h2 className="text-2xl font-bold">Connectez-vous</h2>
       </div>
+      {actionData?.error ? (
+        <em className="text-red-400">{actionData?.error}</em>
+      ) : null}
       <div className="flex flex-col gap-6">
         <div>
           <span className="font-semibold">Email</span>
-          <Input type="email" />
+          <Input name="email" type="email" />
         </div>
         <div>
           <span className="font-semibold">Mot de passe</span>
-          <Input type="password" />
+          <Input name="password" type="password" />
         </div>
       </div>
       <div className="flex gap-3">
@@ -27,4 +36,36 @@ export default function login() {
       </div>
     </Form>
   );
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const session = await getSession(request.headers.get('Cookie'));
+  const data = await loginUser({
+    email: formData.get('email'),
+    password: formData.get('password'),
+  } as LoginUserDto);
+
+  if (data && 'session' in data && data.session?.access_token) {
+    session.set('jwt', data.session.access_token);
+    return redirect('/', {
+      headers: {
+        'Set-Cookie': await commitSession(session),
+      },
+    });
+  }
+
+  if (
+    data &&
+    'error' in data &&
+    data.error === ErrorCodes.INVALID_CREDENTIALS
+  ) {
+    return json({
+      error: "L'email ou mot de passe est invalide",
+    });
+  }
+
+  return json({
+    error: 'Le serveur a rencontré une erreur',
+  });
 }
